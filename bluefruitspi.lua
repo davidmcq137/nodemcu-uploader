728c391d95dc7365e267ecfcc40aa86b5bbe076e
David McQueeney <davidmcq@us.ibm.com>
2019-02-22 00:46:37 -0500

    misc

--- bluefruitspi.lua
+++ bluefruitspi.lua
@@ -90,9 +90,13 @@ function spi_init (id, cs, irq, reset)
    -- We also observed some odd behaviors of the "interrupt" pin called irq,
    -- for example it goes away in the middle of a multi-packet read so you can't
    -- have it gate the while loop (see the code in spi_command)
-   
-   --Reset is active low. Can do via this hw pin or via sw (spi_SDEP_init)
+   --
+   -- I changed the calling convention .. no need to add \n explicitly to AT commands
+   -- low level rountine (spi_cmd) will add it if needed
+   --
+   -- Reset is active low. Can do via this hw pin or via sw (spi_SDEP_init)
    if reset ~= 0 then
+      print("RESETTING BLE FRIEND")
       gpio.mode(reset, gpio.OUTPUT, gpio.PULLUP)
       gpio.write(reset, gpio.LOW)
       tmr.delay(500000)
@@ -119,18 +123,36 @@ function spi_init (id, cs, irq, reset)
 end
 
 
-function spi_cmd(cmd)
-
-   --Executes the supplied AT command, which must be terminated with a new-line character
+function spi_cmd(SDEP_func, cmdP)
+   
+   --parameter SDEP_func can be _SDEP_ATCOMMAND for execution of AT commands
+   --or _SDRP_BLEUART_TX or .._RX for UART functions
+   --Executes the supplied AT command, which may be terminated with a new-line character
    --Returns msgtype, rspid, rsp, which are 8-bit int, 16-bit int and a bytearray
-   --parameter cmd: The new-line-terminated AT command to execute.
+   --parameter cmdP: AT command to execute, or UART input or output
+   --return string will be "OK\r\n" terminated for AT commands
 
-   -- Make sure we stay within the size limit
-   if #cmd > 127 then
+   local buf_tx
+   local cmd
+
+   if SDEP_func ~= _SDEP_ATCOMMAND and SDEP_func ~= _SDEP_BLEUART_TX and
+                    SDEP_func ~= _SDEP_BLEUART_RX and SDEP_func ~= _SDEP_INITIALIZE then
+      print("ERROR: no such _SDEP command type", SDEP_func)
+      return 0,0,nil
+   end
+   
+   cmd = cmdP
+   if not cmd then cmd='' end -- so that #cmd works -- cannot # a nil value      
+
+   if #cmd > 127 then -- shouldn't this be 255?
       print("ERROR: Command too long.")
-      return nil
+      return 0,0,nil
    end
 
+   if SDEP_func == _SDEP_ATCOMMAND and string.byte(cmdP,-1) ~= string.byte("\n") then
+      cmd = cmdP .. "\n"
+   end
+   
    -- for SDEP protocol see Kevin Townsend's docs: https://
    -- learn.adafruit.com/introducing-the-adafruit-bluefruit-spi-breakout/sdep-spi-data-transport
    -- this is a lua/nodemcu port from Kevin's (more elegant!) python implementation:
@@ -139,21 +161,27 @@ function spi_cmd(cmd)
    
    local more = 0x80 -- More bit is in pos 8, 1 = more data available
    local pos = 0     
+   local plen
+   local firstpass=true
 
-   while #cmd - pos > 0 do
+   while #cmd - pos > 0 or firstpass do
 
+      firstpass=false -- if no payload, make sure we do this loop once to send header
+      
       if #cmd - pos <= 16 then  -- Construct the SDEP packet
 	 more = 0               -- more = 0 for last or sole packet
       end
       
-      local plen = #cmd - pos
+      plen = #cmd - pos
       if plen > 16 then
 	 plen = 16
       end
 
+      --print("#cmd, plen, pos:", #cmd, plen, pos)
+      
       -- do little endian pack of the message header parameters
       
-      local buf_tx = struct.pack("<BHB", _MSG_COMMAND, _SDEP_ATCOMMAND, plen+more)
+      buf_tx = struct.pack("<BHB", _MSG_COMMAND, SDEP_func, plen+more)
 
       -- add the payload. use a table so that spi.send know how many elements to send
       -- no way to do that with a string buffer since all chars are legal data
@@ -162,12 +190,19 @@ function spi_cmd(cmd)
       local tx={}          -- table to act as send buffer
       for i = 1, _SDEP_HEADER_LENGTH, 1 do       -- insert params first
 	 tx[i] = string.byte(buf_tx,i)
+	 --print("#", i, tx[i])
       end
-      for i = _SDEP_HEADER_LENGTH + 1, _SDEP_HEADER_LENGTH + plen, 1 do -- then payload
-	 tx[i] = string.byte(string.sub(cmd, pos+1, pos+1+plen), i - _SDEP_HEADER_LENGTH)
+
+      if #cmd > 0 then -- no payload for BLE UART Rx
+	 for i = _SDEP_HEADER_LENGTH + 1, _SDEP_HEADER_LENGTH + plen, 1 do -- then payload
+	    tx[i] = string.byte(string.sub(cmd, pos+1, pos+1+plen), i - _SDEP_HEADER_LENGTH)
+	    --print("@", i, tx[i])
+	 end
       end
       
-      pos = pos + plen
+      --for i=1, _SDEP_HEADER_LENGTH + plen, 1 do
+	-- print(string.format("-->%d 0x%02x %s", i, tx[i], string.char(tx[i]) ) )
+      --end
 
       if g_cs ~= 0 then
 	 gpio.write(g_cs, gpio.LOW)
@@ -180,12 +215,13 @@ function spi_cmd(cmd)
 	 tmr.delay(100)
 	 gpio.write(g_cs, gpio.HIGH)
       end
+      pos = pos + plen
+      --print("bott pos=pos+plen: pos, plen", pos, plen)
       
    end
-   
-   -- Wait for a response (usually not even one trip thru the loop...)
 
-   timeout = 800 -- 800 * 50 usec = 4 msec
+   -- Wait for a response (usually not even one trip thru the loop...)
+   timeout = 800 -- 800 * 50 usec = 40 msec
    while timeout > 0 and not readIRQ() do
       tmr.delay(50)
       timeout = timeout - 1
@@ -193,7 +229,7 @@ function spi_cmd(cmd)
    
    if timeout <= 0 then
       print("ERROR: Timed out waiting for a response.")
-      return nil
+      return 0, 0, nil
    end
    
    -- Retrieve the response message
@@ -227,35 +263,47 @@ function spi_cmd(cmd)
       
       if ll == 1 then -- check on very first read for response code or error
 	 mr=string.byte(buf_rx, 1)
-	 if mr == _MSG_RESPONSE then MSG_resp = true else
+	 
+	 if mr == _MSG_RESPONSE then
+	    MSG_resp = true
+	 else
 	    print("Error response from BLE:", string.format("0x%02x", mr))
-	    return nil
+	    local ss="raw rx: "
+	    local sb
+	    for i=1,20,1 do
+	       sb = string.byte(buf_rx, i)
+	       ss=ss..string.format("%d 0x%02x %s", i, sb, string.char(sb))
+	    end
+	    print(ss)
+	    return 0,0,nil
 	 end
-	 --print("First bye of resp:",string.format("0x%02x ", string.byte(buf_rx,1) ) )
       end
       
       -- Read the message envelope and contents -- note < vs > on Kevin's code
       msgtype, rspid, rsplen = struct.unpack('<BHB', buf_rx)
 
-      --print(string.format("msgtype: 0x%02x, rspid: 0x%02x, rsplen: 0x%02x", msgtype, rspid, rsplen))
+      --print(string.format("%%msgtype: 0x%02x, rspid: 0x%02x, rsplen: 0x%02x", msgtype, rspid, rsplen))
       
-      if rsplen >= _SDEP_BUFFER_LENGTH - _SDEP_HEADER_LENGTH then
+      if rsplen >= _SDEP_BUFFER_LENGTH - _SDEP_HEADER_LENGTH then -- more (0x80) set
 	 rsp = rsp .. string.sub(buf_rx, _SDEP_HEADER_LENGTH+1, _SDEP_BUFFER_LENGTH)
+	 if rsplen == _SDEP_BUFFER_LENGTH - _SDEP_HEADER_LENGTH then break end -- exact fit last one
       else
 	 rsp = rsp .. string.sub(buf_rx, _SDEP_HEADER_LENGTH+1, rsplen+_SDEP_HEADER_LENGTH)
 	 break -- this is the "normal" break when done with the reading as planned
       end
       if ll > 100 then print("ERROR: overrun!") break end -- just in case...
       --tmr.delay(100) -- was 10000 .. seems to run fine without it -- only top one critical
+      tmr.delay(1000)
    end
 
---   print("rsp:\r\n")
---   print(rsp)
---   print("\r\n")
+   --print("bott of loop rsp:\r\n")
+   --print(rsp)
+   --print("\r\n")
+   --print("#rsp:", #rsp)
 
    --for i=1,#rsp,1 do
    --   local rb = string.byte(rsp, i)
-   --   print(string.format("%d 0x%02x '%s'", i, rb, string.char(rb)))
+   --   print(string.format(">>%d 0x%02x '%s'", i, rb, string.char(rb)))
    --end
    
 --   print("\r\n")
@@ -270,35 +318,13 @@ function spi_SDEP_init(callBack)
    -- Sends the SDEP initialize command, which causes the board to reset.
    -- This command should complete in under 1s.
 
-   local buf_tx = struct.pack("<BHB", _MSG_COMMAND, _SDEP_INITIALIZE, 0)
-
-   local tx={}          -- table to act as send buffer
-   for i=1,_SDEP_HEADER_LENGTH,1 do       -- insert headers params -- no payload for this one
-      tx[i] = string.byte(buf_tx,i)
-   end
-
-   local ss="SDEP writing: "
-   for i=1,#tx,1 do
-      ss = ss .. string.format(" %d  0x%02x ", i, string.byte(tx[i])) 
-   end
-   print(ss)
-   
-   if g_cs ~= 0 then
-      gpio.write(g_cs, gpio.LOW)
-      tmr.delay(100)
-   end
-   --
-   spi.send(_HSPI, tx) -- send as table so it's counted by table length
-   --
-   if g_cs ~= 0 then
-      tmr.delay(100)
-      gpio.write(g_cs, gpio.HIGH)
-   end
+   local mt, id, rsp
+   mt, id, rsp = spi_cmd(_SDEP_INITIALIZE)
    
    -- since it may take 1s to reset, allow for a pass-in callback after 1 sec
    if callBack then tmr.create():alarm(1000, tmr.ALARM_SINGLE, callBack) end
 
-   return
+   return rsp
 end
 
 function spi_connected()
@@ -306,17 +332,18 @@ function spi_connected()
    return tonumber(spi_command_check_OK('AT+GAPGETCONN\n')) == 1
 end
 
-function  spi_uart_tx(data)
+function spi_uart_tx(data)
    --Sends the specific bytestring out over BLE UART.
-   return spi_cmd('AT+BLEUARTTX='..data..'\r\n')
+   --works but prob better to use _SDEP_BLEUART_TX command instead
+   return spi_command('AT+BLEUARTTX='..data)
 end
 
 function spi_uart_rx()
-   --Reads byte data from the BLE UART FIFO.
+   --Reads byte data from the BLE UART FIFO
+   --seems not to work .. prob mean to use the _SDEP_BLEUART_RX command instead
    data = spi_command_check_OK('AT+BLEUARTRX')
-   if #data > 0 then
-      -- remove \r\n from ending
-      return string.sub(data,1,-3)
+   if data and #data > 0 then
+      return data
    else
       return nil
    end
@@ -325,15 +352,16 @@ end
 function spi_command(string)
    --Send a command and check response code
    local msgtype, msgid, rsp
-   msgtype, msgid, rsp = spi_cmd(string .. "\n")
-   if not msgtype or msgtype == _MSG_ERROR then
+   msgtype, msgid, rsp = spi_cmd(_SDEP_ATCOMMAND, string)
+   if not rsp or msgtype==0 or msgtype == _MSG_ERROR then
       print("_MSG_ERROR", string.format("0x%02x", msgid))
       return nil
    end
    if msgtype == _MSG_RESPONSE then
       return rsp
    else
-      print("Unknown response or AT command failure", string.format("0x%02x", msgid))
+      print("Unknown response or AT command failure", string.format("Type: 0x%02x", msgtype),
+	    string.format("ID: 0x%02x", msgid))
       return nil
    end
 end
@@ -344,23 +372,27 @@ function spi_command_check_OK(command, dly)
    --Send a fully formed bytestring AT command, and check
    --whether we got an 'OK' back. Returns payload bytes if there are any
    --why on earth would we want dly? make it optional...
+   --returns string with OK\r\n stripped out if it worked, nil if error
 
    ret = spi_command(command)
+   --print("spi_cc_OK - ret from spi_cmd: ")
+   --print(ret)
 
    if dly then tmr.delay(dly) end -- note dly is usec, not msec
 
-   if  #ret == 0 or #string.sub(ret,1, -5) == 0 then
-      print("cc - Not OK")
+   if  ret == nil or #ret == 0 or #string.sub(ret,1, -5) == 0 then
+      print("cc - No return data")
       return nil
    end
    
    if string.sub(ret,-4) ~= 'OK\r\n' then
-      print("cc - Not OK")
+      print("cc - No OK returned")
       return nil
    end
    -- return is 0\r\nOK\r\n or 1\r\nOK\r\n
-   if #string.sub(ret, 1, 1) ~= 0 then
-      return string.sub(ret, 1, 1)
+   -- remove the OK\r\n and return the rest
+   if #string.sub(ret, 1, -5) ~= 0 then
+      return string.sub(ret, 1, -5)
    end
    
    return nil
@@ -371,12 +403,15 @@ end
 
 spi_init (_HSPI, 1, 2, 0) -- alt cs is 1
 
-print("1. spi_cmd('AT+HELP'..'\\n')")
+print("1. spi_cmd(_SDEP_ATCOMMAND, 'AT+BLEGETADDR')")
 local msgtype, rspid, rsp
-msgtype, rspid, rsp = spi_cmd("AT+HELP".."\n")
+msgtype, rspid, rsp = spi_cmd(_SDEP_ATCOMMAND, "AT+BLEGETADDR")
 print("msgtype, rspid, rsp:",
       string.format("0x%02x", msgtype),
       string.format("0x%02x", rspid) )
+
+if not rsp then print("bye") return end
+
 
 print("#rsp:", #rsp)
 print("rsp:")
@@ -390,8 +425,8 @@ end
 print(ss)
 print(" ")
 
-print("2. spi_command_check_OK('ATI'..'\\n', 0)")
-rsp=spi_command_check_OK("ATI".."\n")
+print("2. spi_command_check_OK('ATI')")
+rsp=spi_command_check_OK("ATI")
 print("#rsp:", #rsp)
 print("rsp:")
 print(rsp)
@@ -409,25 +444,101 @@ if spi_connected() then print("connected") else print("not connected") end
 print(" ")
 
 print("4. spi_command(str)")
-
 str = spi_command("ATI")
-
 print("ret:")
 print(str)
 print(" ")
 
-print("5. spi_command('AT+BLEUARTRX')")
+--print("5. spi_command('AT+BLEUARTRX')")
+--str = spi_command("AT+BLEUARTRX")
+--print("ret:")
+--print(str)
+--print(" ")
 
-str = spi_command("AT+BLEUARTRX")
+print("6. spi_command_check_OK('AT+BLEGETRSSI')")
+str= spi_command_check_OK("AT+BLEGETRSSI")
 print("ret:")
 print(str)
 print(" ")
 
-print("6. spi_command_check_OK('AT+HELP)'")
-
-str= spi_command_check_OK("AT+HELP")
+print("7. spi_command_check_OK('AT+HWRANDOM')")
+str= spi_command_check_OK("AT+HWRANDOM")
 print("ret:")
 print(str)
+print(" ")
+
+print("8. spi_command_check_OK('AT+BLEGETADDR')")
+str= spi_command_check_OK("AT+BLEGETADDR")
+print("ret:")
+print(str)
+print(" ")
+
+--print("9. spi_uart_tx(data)")
+--ret = spi_uart_tx("Hello Apple$%&\\r\\n")
+--print("ret:")
+--print(ret)
+--print(" ")
+
+--print("10. spi_uart_tx(data)")
+--ret = spi_uart_tx("Hello Apple Again@#$!!\\r\\n")
+--print("ret:")
+--print(ret)
+--print(" ")
+
+--print("11a. spi_command(AT+BLEUARTFLOW)")
+--ret = spi_command("AT+BLEUARTFLOW")
+--print("ret:")
+--print(ret)
+--print(" ")
+
+--print("11b. spi_command(AT+BLEUARTFLOW=off)")
+--ret = spi_command("AT+BLEUARTFLOW=off")
+--print("ret:")
+--print(ret)
+--print(" ")
+
+--print("11c. spi_command(AT+BLEUARTFLOW)")
+--ret = spi_command("AT+BLEUARTFLOW")
+--print("ret:")
+--print(ret)
+--print(" ")
+
+--print("11c. spi_command(AT+BLEUARTFIFO=TX)")
+--ret = spi_command("AT+BLEUARTFIFO")
+--print("ret:")
+--print(ret)
+--print(" ")
+
+--print("12. spi_uart_rx()")
+--ret = spi_uart_rx()
+--print("ret:")
+--print(ret)
+--print(" ")
+
+local msgtype, rspid, rsp
+
+print("13. spi_cmd(_SDEP_BLEUART_TX, '----')")
+msgtype, rspid, rsp = spi_cmd(_SDEP_BLEUART_TX, "Hello There ios\\r\\n")
+print("msgtype, rspid:", msgtype, rspid)
+print("rsp:")
+print(rsp)
+print(" ")
+
+print("14. spi_cmd(_SDEP_BLEUART_RX)")
+msgtype, rspid, rsp=spi_cmd(_SDEP_BLEUART_RX)
+print("msgtype, rspid:", msgtype, rspid)
+print("rsp:")
+print(rsp)
+print(" ")
+
+for i=1,100,1 do
+   msgtype, rspid, rsp = spi_cmd(_SDEP_BLEUART_TX, "Hello There ios\\r\\n")
+end
+
+
+
+
+
 
 
 
